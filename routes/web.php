e3953dde04badbd7960c6b91a176e53b402a7775
ASUS <tfadriangawa@gmail.com>
2021-12-07 16:42:40 +0700

    fix web route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
